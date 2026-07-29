75d66197ebb414f47ef90181e6cb7032b719b278
makodetushar02-oss <makodetushar02@gmail.com>
2026-07-29 11:20:38 +0530

    Time: 11 ms (88.41%), Space: 16.5 MB (85.51%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
@@ -1,59 +1,58 @@
 class Solution {
 public:
     string smallestPalindrome(string s, int k) {
-        vector<int> freq(26 , 0);
-        for(char c : s){
-            freq[c - 'a']++;
-        }
-        vector<int> half_freq(26 , 0);
+        array<int, 26> freq{};
+        for (char c : s) freq[c - 'a']++;
+        array<int, 26> half_freq{};
         char mid_char = 0;
-        for(int i =0 ; i< 26; ++i){
+        for (int i = 0; i < 26; ++i) {
             half_freq[i] = freq[i] / 2;
-            if(freq[i] % 2 != 0){
-                mid_char = i + 'a';
-            }
+            if (freq[i] & 1) mid_char = static_cast<char>(i + 'a');
         }
-        int n = s.length();
-        int m = n/2;
-        auto get_perms = [&](const vector<int> & f, int target_k) -> long long{
+        const int n = static_cast<int>(s.size());
+        const int m = n / 2;
+        long long K = k;
+        auto get_perms = [](const array<int, 26>& f, long long target_k) -> long long {
             long long res = 1;
-            int total_len = 0 ;
-            for(int count : f){
-                for(int j = 1 ;j<= count ; ++j ){
-                    total_len++;
-                    res = (res * total_len) /j;
-                    if(res > target_k) return target_k +1 ;
+            long long total_len = 0;
+            for (int count : f) {
+                if (count == 0) continue;
+                if (total_len == 0) {
+                    total_len = count;   
+                    continue;
                 }
-            }
-            return res ;
-        } ;
-        long long total_perms = get_perms(half_freq , k);
-        if(total_perms < k ){
-            return "";
-        }
-        string first_half = "";
-        for(int i = 0 ; i < m; ++i){
-            for(int c = 0; c< 26 ; ++c){
-                if( half_freq[c] > 0){
-                    half_freq[c]-- ;
-                    long long p = get_perms(half_freq , k);
-                    if(k <= p){
-                        first_half += (char)(c + 'a');
-                        break ;
-                    }else{
-                        k -= p ;
-                        half_freq[c]++ ; 
-                    }
+                long long N = total_len + count;
+                long long r = min<long long>(count, total_len);
+                for (long long i = 1; i <= r; ++i) {
+                    res = res * (N - r + i) / i;  
+                    if (res > target_k) return target_k + 1;
                 }
+                total_len = N;
+            }
+            return res;
+        };
+        long long total_perms = get_perms(half_freq, K);
+        if (total_perms < K) return "";
+        string first_half;
+        first_half.reserve(m);
+        for (int i = 0; i < m; ++i) {
+            for (int c = 0; c < 26; ++c) {
+                if (half_freq[c] == 0) continue;
+                half_freq[c]--;
+                long long p = get_perms(half_freq, K);
+                if (K <= p) {
+                    first_half += static_cast<char>(c + 'a');
+                    break;
+                }
+                K -= p;
+                half_freq[c]++;
             }
         }
-        string result = first_half;
-        if(n%2 != 0){
-            result += mid_char;
-        }
-        string second_half = first_half;
-        reverse(second_half.begin() , second_half.end());
-        result += second_half;
+        string result;
+        result.reserve(n);
+        result += first_half;
+        if (n & 1) result += mid_char;
+        result.append(first_half.rbegin(), first_half.rend());
         return result;
     }
 };
