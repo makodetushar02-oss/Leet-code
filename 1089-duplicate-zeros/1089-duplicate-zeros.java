29d0ef6dc327c896e2800b0c1a71c5d411082f9e
makodetushar02-oss <makodetushar02@gmail.com>
2026-09-05 19:13:20 +0530

    Time: 1 ms (99.42%), Space: 47 MB (22.97%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int count = 0 ;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 0) count++;
+        }
+        int i = arr.length - 1;
+        int j = arr.length - 1 + count;
+        while(i != j){
+            if(j < arr.length) arr[j] = arr[i];
+            j--;
+            if(arr[i] == 0){
+                if(j < arr.length){
+                arr[j] = arr[i];
+                }
+                j--;
+            }
+            i--;
+        }
+    }
+}
